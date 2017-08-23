27f01b9f6f1a8cbd19bdafb3d883e6a1502965dd
jake64 <2574270078@qq.com>
2017-08-23 18:32:42 +0800

    change background

--- app/src/main/java/com/jinlin/custom/iconcenterview/MainActivity.java
+++ app/src/main/java/com/jinlin/custom/iconcenterview/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jinlin.custom.iconcenterview;
 
+import android.content.Context;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,8 +18,11 @@ public class MainActivity extends BaseActivity{
         setContentView(R.layout.activity_main);
 
         et_search = (EditText) findViewById(R.id.et_search);
-        icet_search = (IconCenterEditText) findViewById(R.id.icet_search);
 
+        icet_search = (IconCenterEditText) findViewById(R.id.icet_search);
+        icet_search.setFocusable(true);
+        icet_search.setFocusableInTouchMode(true);
+        icet_search.requestFocus();
         // 实现TextWatcher监听即可
         icet_search.setOnSearchClickListener(new IconCenterEditText.OnSearchClickListener() {
             @Override
